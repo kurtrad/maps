9ae194ab0fb42e85ef2521fbe5f49566037a056a
Kurt Radamaker <kurtrad@mexicobirding.com>
2018-02-17 22:34:01 -0700

    Commit

---
+++ ebirdApi/js/tools/CheckVersion.js
@@ -0,0 +1,57 @@
+﻿///////////////////////////////////////////////////////////////////////////
+// Copyright © 2015 Republic Services Inc. All Rights Reserved.
+//
+// Licensed under the Apache License Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Description: check to see if the user has cleared cach since loading a previous version
+// if not they are redirected to the version.html page
+//
+///////////////////////////////////////////////////////////////////////////////
+// Code Changes                                                              //
+// <USERID> <Change ID> <DATE> <DESCRIPTION>                                 //
+// handeka  US642 Feb 11/2016 initial version                                //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+define([
+      "dojo/_base/declare"
+
+], function (
+    declare
+) {
+    return declare(null, {
+        checkLocalStorageVersion: function () {
+            var versionlocalStorage;
+            var blnReturn = true
+            var versionKey = "1";
+            var currentVersion = "1";
+
+            // This checks the browser for Local Storage support - if not supported the unsupported version check will forward user to upgrade page
+            if (typeof (Storage) !== "undefined") {
+                var versionlocalStorage = localStorage.getItem(versionKey);
+                if (app.util.isNull(versionlocalStorage)) {
+                    //nothing in the cache just set the version
+                    versionlocalStorage = currentVersion;
+                    localStorage.setItem(versionKey, versionlocalStorage);
+                }
+
+                if (!("1" == versionlocalStorage)) {
+                    //new version, user must clear the cache
+                    blnReturn = false
+                }
+
+            }
+            return blnReturn;
+        }
+    });
+});
---
+++ ebirdApi/loadspecies.js
@@ -0,0 +1,144 @@
+﻿/*************************
+* Create a point graphic
+*************************/
+
+define([
+    "esri/geometry/Point"
+],function (
+    point
+) {
+            loadspecies= function () {
+                for (i = 0; i < data.length; ++i) {
+                    review = "";
+                    var point = new Point(data[i].lng, data[i].lat);
+
+                    $(ABCdata).find('Species').each(function () {
+                        var species = $(this).attr('EnglishName');
+
+                        if (data[i].comName == species) {
+
+                            review = $(this).attr('Review');
+                        }
+
+                        switch (review) {
+                            case "Y":
+                                reviewSpecies = "Y"
+                                break;
+                            case "N":
+                                sketchSpecies = "Y"
+                                break;
+                            default:
+                                notableSpecies = "Y"
+                        }
+                    })
+
+
+                    // Create a symbol for drawing the point
+                    var markerSymbol = {
+                        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+                        color: [0, 128, 0], //green
+                        outline: { // autocasts as new SimpleLineSymbol()
+                            color: [0, 100, 0],
+                            width: 2
+                        }
+                    };
+
+                    if (review == "Y") {
+                        // Create a symbol for drawing the point
+                        var markerSymbol = {
+                            type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+                            color: [255, 0, 0], //red
+                            outline: { // autocasts as new SimpleLineSymbol()
+                                color: [128, 0, 0],
+                                width: 2
+                            }
+                        };
+
+                        review = " "
+                    }
+                    if (review == "N") {
+                        // Create a symbol for drawing the point
+                        var markerSymbol = {
+                            type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+                            color: [0, 0, 255], //blue
+                            outline: { // autocasts as new SimpleLineSymbol()
+                                color: [65, 105, 255],
+                                width: 2
+                            }
+                        };
+
+                        review = " "
+                    }
+
+
+                    // Create an object for storing attributes related to the line
+                    var pointAtt = {
+                        Name: data[i].comName,
+                        Date: data[i].obsDt,
+                        Location: data[i].locName,
+                        Nunmber: data[i].howMany,
+                        Observer: data[i].firstName + ' ' + data[i].lastName
+                    };
+
+                    /*******************************************
+                     * Create a new graphic and add the geometry,
+                     * symbol, and attributes to it. You may also
+                     * add a simple PopupTemplate to the graphic.
+                     * This allows users to view the graphic's
+                     * attributes when it is clicked.
+                     ******************************************/
+
+                    var pointGraphic = new Graphic({
+                        geometry: point,
+                        symbol: markerSymbol,
+                        attributes: pointAtt,
+                        popupTemplate: {  // autocasts as new PopupTemplate()
+                            title: "{Name}",
+                            content: [{
+                                type: "fields",
+                                fieldInfos: [{
+                                    fieldName: "Name"
+                                }, {
+                                    fieldName: "Date"
+                                }, {
+                                    fieldName: "Location"
+                                }, {
+                                    fieldName: "Number"
+                                }, {
+                                    fieldName: "Observer"
+                                }]
+                            }]
+                        }
+                    });
+
+                    switch (selection) {
+                        case "Review Species":
+                            if (reviewSpecies == "Y") {
+                                show = "Y"
+                            }
+                            break;
+                        case "Sketch Species":
+                            if (sketchSpecies == "Y") {
+                                show = "Y"
+                            }
+                            break;
+                        case "Notable Species":
+                            if (notableSpecies == "Y") {
+                                show = "Y"
+                            }
+                            break;
+                        default:
+                            show = "Y"
+                    }
+
+                    if (show == "Y") {
+                        // Add the line graphic to the view's GraphicsLayer
+                        view.graphics.add(pointGraphic);
+                    }
+
+                }
+            }
+}
+    
+        );
+
